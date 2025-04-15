e39c3a2b87f2edb949605fe8794a97a5437ab89f
Marco Antonio Alducin Garcia <sokram_r5_1992@hotmail.com>
2025-04-15 00:58:28 -0600

    modulo inicio de sesion: inclusion de formulario, Inclusion de CORS para evitar problemas con consumo de servicios API REST, metodo de consulta para correo registrado

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,66 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import axios from 'axios';
+import bcrypt from "bcryptjs-react";
 
 function App() {
-  const baseUrl = 'http://localhost:8888/apiFrameworks/';
+  const baseUrl = 'http://localhost:8888/api';
   const [data, setData] = useState([]);
+  const [showHide, setShowHide] = useState(false);
+  const [mensaje, setMensaje] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
-  const peticionGet = async() => {
-    await axios.get(baseUrl)
+  const MENSAJE_CORREO_NO_EXISTE = "El correo no existe";
+  const MENSAJE_NO_COINCIDEN = "El password y la contraseña no coinciden";
+
+  console.log(data, "redirigir: "+redirect)
+
+  const iniciarSesion = (event) => {
+    event.preventDefault();
+    let pass = event.target.contraseña.value;
+    let email = event.target.email.value;
+
+    peticionGet(email, pass);
+  };
+
+  const peticionGet = async(email, pass) => {
+    await axios.get(baseUrl+'?email="'+email+'"')
     .then(response => {
-      setData(response.data);
+      if(response.data){
+        bcrypt.compare(pass, response.data['password'], function(err, res) {
+          if(res){
+            setShowHide(false);
+            setRedirect(res);
+            setData(response.data);
+          }
+          else{
+            setRedirect(err);
+            setShowHide(true);
+            setMensaje(MENSAJE_NO_COINCIDEN);
+          }
+        })
+      }
+      else{
+        setShowHide(true);
+        setMensaje(MENSAJE_CORREO_NO_EXISTE);
+      }
     })
     .catch(error => {
       console.log(error);
     })
   }
-  
 
   return (
-    <h1 class="text-3xl font-bold underline text-pink-300">
-      Hello world!
-    </h1>
+    <Fragment>
+      <form onSubmit={iniciarSesion}>
+        <input type="password" id="contraseña" name="contraseña" required={true} placeholder="Contraseña" />
+        <input type="email" id="email" name="email" required={true} placeholder="correo" />
+        <button type='submit' >Iniciar sesion</button>
+      </form>
+      {showHide && 
+      <>
+        <h1>{mensaje}</h1>
+      </>}
+    </Fragment>
 
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Inclusion de CORS para evitar problemas con consumo de servicios API REST
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+cors(corsOptions);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
